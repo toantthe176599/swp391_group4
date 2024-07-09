9bae62c4b7cbf32eb3eaf6f86ed369f1efbe2766
Trần Tú Toàn <toantthe176599@fpt.edu.vn>
2024-07-09 23:00:19 +0700

    Update renderTransactionDashboard.java

--- src/java/controller/admin/dashboard/renderTransactionDashboard.java
+++ src/java/controller/admin/dashboard/renderTransactionDashboard.java
@@ -38,36 +38,10 @@ public class renderTransactionDashboard extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        // get id event from url
-        String pathInfo = req.getPathInfo();
-        String[] pathSegments = pathInfo.split("/");
-        String eventId = pathSegments[pathSegments.length - 1];
-        //end  
 
-        // get detail transaction
-        queryBooking qBooking = queryBooking.createInstanceBooking();
-        List<ReportTransaction> reportTran = qBooking.getBookingByEvent(eventId);
-        req.setAttribute("report", reportTran);
-        //end
-        req.getRequestDispatcher("/views/admin/pages/Dashboard/transaction.jsp").forward(req, res);
     }
 
     /**
