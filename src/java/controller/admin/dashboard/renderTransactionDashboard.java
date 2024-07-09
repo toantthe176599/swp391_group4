61f4da70c7716b1d3ef74ec547691385c42f7e45
Trần Tú Toàn <toantthe176599@fpt.edu.vn>
2024-07-09 23:01:12 +0700

    Update renderTransactionDashboard.java

--- src/java/controller/admin/dashboard/renderTransactionDashboard.java
+++ src/java/controller/admin/dashboard/renderTransactionDashboard.java
@@ -41,7 +41,33 @@ public class renderTransactionDashboard extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-
+// get id event from url
+      String pathInfo = req.getPathInfo();
+        String[] pathSegments = pathInfo.split("/");
+        String eventId = pathSegments[pathSegments.length - 1];
+        
+        // get detail transaction
+        queryBooking qBooking = queryBooking.createInstanceBooking();
+        List<ReportTransaction> reportTran = qBooking.getBookingByEvent(eventId);
+               
+         // Phân trang
+        int pageSize = 3; // Số lượng bản ghi trên mỗi trang
+        int page;
+        String pageStr = req.getParameter("page");
+        if (pageStr == null) {
+            page = 1; // Mặc định là trang đầu tiên
+        } else {
+            page = Integer.parseInt(pageStr);
+        }
+        int totalRecords = reportTran.size();
+        int totalPages = (int) Math.ceil((double) totalRecords / pageSize);
+        int start = (page - 1) * pageSize;
+        int end = Math.min(start + pageSize, totalRecords);
+        List<ReportTransaction> paginatedList = reportTran.subList(start, end);
+        req.setAttribute("report", paginatedList);
+        req.setAttribute("currentPage", page);
+        req.setAttribute("totalPages", totalPages);
+        req.getRequestDispatcher("/views/admin/pages/Dashboard/transaction.jsp").forward(req, res);
     }
 
     /**
